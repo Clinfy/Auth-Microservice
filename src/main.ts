8461ef24c578bee87ad675016e127bd771bc5586
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-03 18:15:40 -0300

    Set up Swagger documentation for Clinify Users Microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+  //Swagger
+    const config = new DocumentBuilder()
+        .setTitle('Clinify Users Microservice')
+        .setDescription('Docs')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
