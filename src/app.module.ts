d0af73f65e90b9abc5b88e6583b81c255106bdcc
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-03 13:40:24 -0300

    Configure TypeORM with PostgreSQL and global configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {entities} from "./entities";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+            type: 'postgres',
+            url: configService.get('DATABASE_HOST'),
+            entities: [...entities],
+            synchronize: true,
+        }),
+    }),
+    TypeOrmModule.forFeature(entities),
+],
+controllers: [AppController],
+providers: [AppService],
 })
 export class AppModule {}
