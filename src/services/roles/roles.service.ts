2f866f8b1ecc31f6521236852b89d1872379da9e
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-03 23:11:28 -0300

    Implement RolesController and RolesService with CRUD operations and permission management

--- src/services/roles/roles.service.ts
+++ src/services/roles/roles.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {RoleEntity} from "src/entities/role.entity";
+import {PermissionsService} from "src/services/permissions/permissions.service";
+import {CreateRoleDTO} from "src/interfaces/DTO/create.dto";
+import {AssignPermissionDTO} from "src/interfaces/DTO/assign.dto";
+import {PatchRoleDTO} from "src/interfaces/DTO/patch.dto";
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+    constructor(
+        @InjectRepository(RoleEntity)
+        private readonly roleRepository: Repository<RoleEntity>,
+
+        private readonly permissionService: PermissionsService,
+    ) {}
+
+    async create(dto: CreateRoleDTO): Promise<RoleEntity> {
+        return await this.roleRepository.save(this.roleRepository.create(dto));
+    }
+
+    async update(id: number, dto: PatchRoleDTO): Promise<RoleEntity> {
+        return await this.roleRepository.save(this.roleRepository.merge(await this.findOne(id),dto));
+    }
+
+    async delete(id: number): Promise<{ message: string }> {
+        const role = await this.findOne(id);
+        await this.roleRepository.remove(role);
+        return {message: `Role ${role.name} deleted`};
+    }
+
+    async findOne(id: number): Promise<RoleEntity> {
+        const role = await this.roleRepository.findOneBy({id});
+        if(!role) throw new NotFoundException('Role not found');
+        return role;
+    }
+
+    async findAll(): Promise<RoleEntity[]> {
+        return await this.roleRepository.find();
+    }
+
+    async assignPermissions(roleId: number, dto: AssignPermissionDTO): Promise<RoleEntity> {
+        const role = await this.findOne(roleId);
+        role.permissions = await Promise.all(dto.permissionIds.map(id => this.permissionService.findOne(id)));
+        return await this.roleRepository.save(role);
+    }
+}
