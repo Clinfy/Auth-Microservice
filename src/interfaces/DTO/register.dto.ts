3c1b221d9484cd4ac94d7d6e6962808c6ff82040
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-04 19:28:57 -0300

    Add unique email validation to RegisterUserDTO

--- src/interfaces/DTO/register.dto.ts
+++ src/interfaces/DTO/register.dto.ts
@@ -1,8 +1,10 @@
 import {IsNotEmpty, IsString} from "class-validator";
+import {IsUniqueEmail} from "src/common/validators/unique-email.validator";
 
 export class RegisterUserDTO {
     @IsString()
     @IsNotEmpty()
+    @IsUniqueEmail()
     email: string;
 
     @IsString()
