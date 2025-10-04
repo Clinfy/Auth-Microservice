d146f4c55e0d2927a686174871de72d746b2d9e4
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-03 22:32:08 -0300

    Implement CRUD operations for permissions in PermissionsController and PermissionsService

--- src/services/permissions/permissions.controller.ts
+++ src/services/permissions/permissions.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Param, Post, UseGuards} from '@nestjs/common';
+import {PermissionsService} from "src/services/permissions/permissions.service";
+import {AuthGuard} from "src/middlewares/auth.middleware";
+import {Permissions} from "src/middlewares/decorators/permissions.decorator";
+import {ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation} from "@nestjs/swagger";
+import {PermissionEntity} from "src/entities/permission.entity";
+import {CreatePermissionDTO} from "src/interfaces/DTO/create.dto";
 
 @Controller('permissions')
-export class PermissionsController {}
+export class PermissionsController {
+    constructor(private readonly permissionService: PermissionsService) {}
+
+    @UseGuards(AuthGuard)
+    @Permissions(['PERMISSIONS_CREATE'])
+    @Post('new')
+    @ApiOperation({summary: 'Create a new permission'})
+    @ApiCreatedResponse({type: PermissionEntity})
+    create(@Body() dto: CreatePermissionDTO): Promise<PermissionEntity> {
+        return this.permissionService.create(dto);
+    }
+
+    @UseGuards(AuthGuard)
+    @Permissions(['PERMISSIONS_UPDATE'])
+    @Post('edit/:id')
+    @ApiOperation({summary: 'Update a permission'})
+    @ApiOkResponse({type: PermissionEntity})
+    @ApiNotFoundResponse({description: 'Permission not found'})
+    edit(@Body() dto: CreatePermissionDTO, @Param('id') id: number): Promise<PermissionEntity> {
+        return this.permissionService.update(id, dto);
+    }
+
+    @UseGuards(AuthGuard)
+    @Permissions(['PERMISSIONS_DELETE'])
+    @Post('delete/:id')
+    @ApiOperation({summary: 'Delete a permission'})
+    @ApiOkResponse({description: 'Permission deleted'})
+    @ApiNotFoundResponse({description: 'Permission not found'})
+    delete(@Param('id') id: number): Promise<{message: string}> {
+        return this.permissionService.delete(id);
+    }
+
+    @UseGuards(AuthGuard)
+    @Permissions(['PERMISSIONS_READ'])
+    @Post('find/:id')
+    @ApiOperation({summary: 'Find a permission by id number'})
+    @ApiOkResponse({type: PermissionEntity})
+    @ApiNotFoundResponse({description: 'Permission not found'})
+    findOne(@Param('id') id: number): Promise<PermissionEntity> {
+        return this.permissionService.findOne(id);
+    }
+
+    @UseGuards(AuthGuard)
+    @Permissions(['PERMISSIONS_READ'])
+    @Post('all')
+    @ApiOperation({summary: 'Find all permissions'})
+    @ApiOkResponse({type: [PermissionEntity]})
+    findAll(): Promise<PermissionEntity[]> {
+        return this.permissionService.findAll();
+    }
+}
--- src/services/permissions/permissions.service.ts
+++ src/services/permissions/permissions.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
+import {PermissionEntity} from "src/entities/permission.entity";
+import {CreatePermissionDTO} from "src/interfaces/DTO/create.dto";
+import {PatchPermissionDTO} from "src/interfaces/DTO/patch.dto";
 
 @Injectable()
-export class PermissionsService {}
+export class PermissionsService {
+    constructor(
+        @InjectRepository(PermissionEntity)
+        private readonly permissionRepository: Repository<PermissionEntity>,
+    ) {}
+
+    async create(dto: CreatePermissionDTO): Promise<PermissionEntity> {
+        return await this.permissionRepository.save(this.permissionRepository.create(dto));
+    }
+
+    async update(id: number, dto: PatchPermissionDTO): Promise<PermissionEntity> {
+        return await this.permissionRepository.save(this.permissionRepository.merge(await this.findOne(id),dto));
+    }
+
+    async delete(id: number): Promise<{ message: string }> {
+        const permission = await this.findOne(id);
+        await this.permissionRepository.remove(permission);
+        return {message: `Permission ${permission.code} deleted`};
+    }
+
+    async findOne(id: number): Promise<PermissionEntity> {
+        const permission = await this.permissionRepository.findOneBy({id});
+        if(!permission) throw new NotFoundException('Permission not found');
+        return permission;
+    }
+
+    async findAll(): Promise<PermissionEntity[]> {
+        return await this.permissionRepository.find();
+    }
+}
