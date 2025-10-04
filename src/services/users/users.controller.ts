327c42c9526f0c2c1d10129e32562136dd7fd166
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-04 01:16:33 -0300

    Implement user registration, login, and role assignment functionalities in UsersController and UsersService

--- src/services/users/users.controller.ts
+++ src/services/users/users.controller.ts
@@ -1,4 +1,70 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Req, UseGuards} from '@nestjs/common';
+import {AuthGuard} from "src/middlewares/auth.middleware";
+import {Permissions} from "src/middlewares/decorators/permissions.decorator";
+import {ApiBearerAuth, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation} from "@nestjs/swagger";
+import * as requestUser from "src/interfaces/request-user";
+import {UsersService} from "src/services/users/users.service";
+import {UserEntity} from "src/entities/user.entity";
+import {RegisterUserDTO} from "src/interfaces/DTO/register.dto";
+import {LoginDTO} from "src/interfaces/DTO/login.dto";
+import {AssignRoleDTO} from "src/interfaces/DTO/assign.dto";
+import {AuthInterface} from "src/interfaces/auth.interface";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @UseGuards(AuthGuard)
+    @Permissions(['USERS_CREATE'])
+    @ApiOperation({summary: 'Create a new user'})
+    @ApiBearerAuth()
+    @ApiCreatedResponse({type: UserEntity})
+    @Post('register')
+    register(@Body() dto: RegisterUserDTO): Promise<UserEntity> {
+        return this.userService.register(dto);
+    }
+
+    @ApiOperation({summary: 'Log in a user'})
+    @ApiOkResponse({schema: {type: 'object', properties: { accessToken: { type: 'string' }, refreshToken: { type: 'string' }}}})
+    @Post('login')
+    logIn(@Body() dto: LoginDTO): Promise<AuthInterface> {
+        return this.userService.logIn(dto);
+    }
+
+    @ApiOperation({summary: 'Refresh a user token'})
+    @ApiOkResponse({schema: {type: 'object', properties: { accessToken: { type: 'string' }, refreshToken: { type: 'string' }}}})
+    @Get('refresh-token')
+    refreshToken(@Req() request: Request): Promise<AuthInterface> {
+        return this.userService.refreshToken(request.headers['refresh-token'] as string);
+    }
+
+    @UseGuards(AuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({summary: 'Return if a user have permissions to do something'})
+    @ApiOkResponse({type: Boolean})
+    @Get('can-do')
+    canDo(@Req() request: requestUser.RequestWithUser, @Param('permission') permission: string ): Promise<Boolean> {
+        return this.userService.canDo(request.user, permission);
+    }
+
+    @UseGuards(AuthGuard)
+    @ApiBearerAuth()
+    @ApiOperation({summary: 'Return the email of the user logged in'})
+    @ApiOkResponse({schema: {type: 'object', properties: { email: { type: 'string' }}}})
+    @Get('me')
+    me(@Req() request: requestUser.RequestWithUser): Promise<string> {
+        return Promise.resolve(request.user.email);
+    }
+
+    @UseGuards(AuthGuard)
+    @Permissions(['USERS_UPDATE'])
+    @ApiBearerAuth()
+    @ApiOperation({summary: 'Assign roles to a user'})
+    @ApiOkResponse({type: UserEntity})
+    @ApiNotFoundResponse({description: 'User not found'})
+    @ApiNotFoundResponse({description: 'Role not found'})
+    @Post('assign-role/:id')
+    assignRole(@Param('id') id: number, @Body() dto: AssignRoleDTO): Promise<UserEntity> {
+        return this.userService.assignRole(id, dto);
+    }
+}
--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,4 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException, UnauthorizedException} from '@nestjs/common';
+import {InjectDataSource, InjectRepository} from "@nestjs/typeorm";
+import {UserEntity} from "src/entities/user.entity";
+import {DataSource, Repository} from "typeorm";
+import {JwtService} from "src/services/JWT/jwt.service";
+import {RegisterUserDTO} from "src/interfaces/DTO/register.dto";
+import {UserI} from "src/interfaces/user.interface";
+import { compareSync } from 'bcrypt';
+import {LoginDTO} from "src/interfaces/DTO/login.dto";
+import {AuthInterface} from "src/interfaces/auth.interface";
+import {AssignRoleDTO} from "src/interfaces/DTO/assign.dto";
+import {RolesService} from "src/services/roles/roles.service";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+
+        @InjectDataSource()
+        private readonly dataSource: DataSource,
+
+        private readonly jwtService: JwtService,
+        private readonly roleService: RolesService,
+    ) {}
+
+    async refreshToken(refreshToken: string): Promise<AuthInterface> {
+        return this.jwtService.refreshToken(refreshToken);
+    }
+
+    async canDo(user: UserI, permissionCode: string): Promise<boolean> {
+        const result = user.permissionCodes.includes(permissionCode);
+        if (!result) {
+            throw new UnauthorizedException('No cuenta con permisos suficientes para realizar esa acción');
+        }
+        return result;
+    }
+
+    async register(dto: RegisterUserDTO): Promise<UserEntity> {
+        try {
+            return await this.dataSource.transaction(async manager => {
+                const user = this.userRepository.create(dto);
+                return await manager.save(user);
+            })
+        }catch (error) {
+            throw new Error(error);
+        }
+    }
+
+    async logIn(body: LoginDTO): Promise<AuthInterface> {
+        const user = await this.findByEmail(body.email);
+        if(!user) throw new UnauthorizedException('Wrong email or password');
+
+        if(!user.active) throw new UnauthorizedException('This user is not active')
+
+        const compare = compareSync(body.password, user.password);
+        if(!compare) throw new UnauthorizedException('Wrong email or password');
+
+        return {
+            accessToken: this.jwtService.generateToken({email: user.email}, 'auth'),
+            refreshToken: this.jwtService.generateToken({email: user.email}, 'refresh')
+        }
+    }
+
+    async findByEmail(email: string): Promise<UserEntity> {
+            const user = await this.userRepository.findOne({where: {email}});
+            if(!user) throw new NotFoundException(`El email ${email} no se encuentra registrado`);
+            return user;
+    }
+
+    async assignRole(id: number, dto: AssignRoleDTO): Promise<UserEntity> {
+        const user = await this.findOne(id);
+        user.roles = await Promise.all(dto.rolesIds.map(id => this.roleService.findOne(id)));
+        return await this.userRepository.save(user);
+    }
+
+    private async findOne(id: number): Promise<UserEntity> {
+        const user = await this.userRepository.findOneBy({id});
+        if(!user) throw new NotFoundException('User not found');
+        return user;
+    }
+}
