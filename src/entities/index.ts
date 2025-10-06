a9d48328821f889b8265af14131850dc6375a0de
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-05 21:30:35 -0300

    Add ApiKeyEntity to the list of exported entities

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,9 +1,11 @@
-import {UserEntity} from "./user.entity";
-import {RoleEntity} from "./role.entity";
-import {PermissionEntity} from "./permission.entity";
+import { UserEntity } from "./user.entity";
+import { RoleEntity } from "./role.entity";
+import { PermissionEntity } from "./permission.entity";
+import { ApiKeyEntity } from "./api-key.entity";
 
 export const entities = [
     UserEntity,
     RoleEntity,
-    PermissionEntity
-]
+    PermissionEntity,
+    ApiKeyEntity,
+];
