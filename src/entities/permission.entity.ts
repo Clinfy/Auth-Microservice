c297ac288fe8086c08f1f33df9d19447000db7e9
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-05 21:30:30 -0300

    Add many-to-many relationship between PermissionEntity and ApiKeyEntity

--- src/entities/permission.entity.ts
+++ src/entities/permission.entity.ts
@@ -1,14 +1,18 @@
-import {BaseEntity, Column, Entity, ManyToMany, PrimaryGeneratedColumn} from "typeorm";
-import {RoleEntity} from "./role.entity";
+import { BaseEntity, Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { RoleEntity } from "./role.entity";
+import { ApiKeyEntity } from "./api-key.entity";
 
 @Entity('permission')
 export class PermissionEntity extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column({unique: true})
+    @Column({ unique: true })
     code: string;
 
     @ManyToMany(() => RoleEntity, role => role.permissions)
     roles: RoleEntity[];
+
+    @ManyToMany(() => ApiKeyEntity, apiKey => apiKey.permissions)
+    api_keys: ApiKeyEntity[];
 }
