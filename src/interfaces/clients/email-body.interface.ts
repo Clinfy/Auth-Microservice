657ae5a198553583a02f375912f9ed9c0e525d07
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-07 09:03:40 -0300

    Add EmailService to handle password reset emails and define EmailBody interface

---
+++ src/interfaces/clients/email-body.interface.ts
@@ -0,0 +1,5 @@
+export interface EmailBody {
+  recipient: string[];
+  subject: string;
+  html: string;
+}
---
+++ src/services/email/email.service.ts
@@ -0,0 +1,38 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from 'src/services/JWT/jwt.service';
+import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
+import { EmailBody } from 'src/interfaces/clients/email-body.interface';
+
+@Injectable()
+export class EmailService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async sendResetPasswordMail(email: string) {
+    const payload = { email };
+    const token = this.jwtService.generateToken(payload,'resetPassword')
+
+    const resetPasswordUrl = `${this.configService.get('FRONTEND_URL')}/reset-password?token=${token}`;
+
+    const recipient = [email];
+    const subject = 'Reset Password of your Clinfy Account';
+    const html = `Click the link below to reset your Clinfy password: ${resetPasswordUrl} \nthis link will expire in 15 minutes`;
+
+    const body = {recipient,subject,html} as EmailBody;
+
+    await axios.post(`${this.configService.get('EMAIL_API_URL')}/email/send`, {
+      body
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': this.configService.get('EMAIL_API_KEY'),
+      },
+    }).catch(error => {
+      console.error('Error sending email:', error);
+      throw new BadRequestException('Failed to send email');
+    })
+  }
+}
