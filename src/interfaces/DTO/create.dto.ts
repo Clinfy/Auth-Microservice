5167c9c780b4b901f0764e82aa7121b9af857da7
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-13 14:37:15 -0300

    Update DTO validation error messages for consistency and clarity

--- src/interfaces/DTO/create.dto.ts
+++ src/interfaces/DTO/create.dto.ts
@@ -3,15 +3,15 @@ import { IsUniquePermissionCode } from 'src/common/validators/unique-permission-
 import { IsUniqueRoleName } from 'src/common/validators/unique-role-name.validator';
 
 export class CreatePermissionDTO {
-    @IsString({message: 'El dato debe ser una cadena de texto string'})
-    @IsNotEmpty({message: 'El dato no puede estar vacio'})
+    @IsString({message: 'Code must be a string'})
+    @IsNotEmpty({message: 'This field is required'})
     @IsUniquePermissionCode()
     code: string;
 }
 
 export class CreateRoleDTO {
-    @IsString({message: 'El dato debe ser una cadena de texto string'})
-    @IsNotEmpty({message: 'El dato no puede estar vacio'})
+    @IsString({message: 'Name must be a string'})
+    @IsNotEmpty({message: 'This field is required'})
     @IsUniqueRoleName()
     name: string;
 }
--- src/interfaces/DTO/login.dto.ts
+++ src/interfaces/DTO/login.dto.ts
@@ -1,11 +1,11 @@
 import {IsNotEmpty, IsString} from "class-validator";
 
 export class LoginDTO {
-    @IsString()
-    @IsNotEmpty()
+    @IsString({message: 'Your email must be a string'})
+    @IsNotEmpty({message: 'This field is required'})
     email: string;
 
-    @IsString()
-    @IsNotEmpty()
+    @IsString({message: 'Your password must be a string'})
+    @IsNotEmpty({message: 'This field is required'})
     password: string;
 }
