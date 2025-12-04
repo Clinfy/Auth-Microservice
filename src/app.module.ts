7665cf9ddf73d0d915e424ff19bfab8a65441311
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-12-04 12:53:17 -0300

    Add RabbitMQ setup for `EMAIL_SERVICE` in `AppModule`

--- src/app.module.ts
+++ src/app.module.ts
@@ -50,6 +50,21 @@ imports: [ConfigModule.forRoot({
           }
         }
       })
+    },
+    {
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      name: 'EMAIL_SERVICE',
+      useFactory: async (configService: ConfigService) => ({
+        transport: Transport.RMQ,
+        options: {
+          urls: [configService.get<string>('RABBITMQ_URL') as string],
+          queue: 'email_queue',
+          queueOptions: {
+            durable: true
+          }
+        }
+      })
     }
   ]),
 
