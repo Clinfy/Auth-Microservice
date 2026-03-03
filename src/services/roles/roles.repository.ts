50ae09be4454dff83160598f1cdca11c34e1be87
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-02 22:12:09 -0300

    Add `RolesRepository` for managing `RoleEntity` operations

---
+++ src/services/roles/roles.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RoleEntity } from 'src/entities/role.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class RolesRepository {
+  constructor(
+    @InjectRepository(RoleEntity)
+    private readonly ormRepository: Repository<RoleEntity>
+  ) {}
+  
+  async save(role: Partial<RoleEntity>): Promise<RoleEntity> {
+    return await this.ormRepository.save(role);
+  }
+  
+  async merge (id: string, changes: Partial<RoleEntity>): Promise<RoleEntity> {
+    return this.ormRepository.merge(<RoleEntity> await this.ormRepository.findOneBy({id}), changes);
+  }
+  
+  async findOneById(id: string): Promise<RoleEntity | null> {
+    return await this.ormRepository.findOneBy({id});
+  }
+  
+  async findAll(): Promise<RoleEntity[]> {
+    return await this.ormRepository.find();
+  }
+
+  async remove (role: RoleEntity): Promise<void> {
+    await this.ormRepository.remove(role);
+  }
+}
