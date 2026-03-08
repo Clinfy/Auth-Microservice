c44ac456d96c2656c037fe14a298771aad4ee21a
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-08 00:31:25 -0300

    Standardize error handling in `permissions.service` by introducing `PermissionsException` and `PermissionsErrorCodes` for improved consistency and clarity.

---
+++ src/services/permissions/permissions.exception.handler.ts
@@ -0,0 +1,14 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export enum PermissionsErrorCodes {
+  CREATE_PERMISSION_FAILED = 'CREATE_PERMISSION_FAILED',
+  UPDATE_PERMISSION_FAILED = 'UPDATE_PERMISSION_FAILED',
+  DELETE_PERMISSION_FAILED = 'DELETE_PERMISSION_FAILED',
+  PERMISSION_NOT_FOUND = 'PERMISSION_NOT_FOUND',
+}
+
+export class PermissionsException extends HttpException {
+  constructor(message: string, errorCode: PermissionsErrorCodes, status: HttpStatus) {
+    super({ message, errorCode, statusCode: status }, status);
+  }
+}
--- src/services/permissions/permissions.service.ts
+++ src/services/permissions/permissions.service.ts
@@ -1,42 +1,75 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { PermissionEntity } from 'src/entities/permission.entity';
 import { CreatePermissionDTO } from 'src/interfaces/DTO/create.dto';
 import { PatchPermissionDTO } from 'src/interfaces/DTO/patch.dto';
 import { RequestWithUser } from 'src/interfaces/request-user';
 import { PermissionsRepository } from 'src/services/permissions/permissions.repository';
+import { PermissionsErrorCodes, PermissionsException } from 'src/services/permissions/permissions.exception.handler';
 
 @Injectable()
 export class PermissionsService {
   constructor(private readonly permissionRepository: PermissionsRepository) {}
 
   async create(dto: CreatePermissionDTO, request: RequestWithUser): Promise<PermissionEntity> {
-    return await this.permissionRepository.save(
-      this.permissionRepository.create({
-        ...dto,
-        created_by: request.user,
-      }),
-    );
+    try {
+      return await this.permissionRepository.save(
+        this.permissionRepository.create({
+          ...dto,
+          created_by: request.user,
+        }),
+      );
+    } catch (error) {
+      throw new PermissionsException(
+        'Permission not created',
+        PermissionsErrorCodes.CREATE_PERMISSION_FAILED,
+        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async update(id: string, dto: PatchPermissionDTO): Promise<PermissionEntity> {
-    return await this.permissionRepository.save(
-      await this.permissionRepository.merge(await this.findOne(id), dto),
-    );
+    try {
+      return await this.permissionRepository.save(
+        await this.permissionRepository.merge(await this.findOne(id), dto),
+      );
+    } catch (error) {
+      throw new PermissionsException(
+        'Permission not updated',
+        PermissionsErrorCodes.UPDATE_PERMISSION_FAILED,
+        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async delete(id: string): Promise<{ message: string }> {
-    const permission = await this.findOne(id);
-    await this.permissionRepository.remove(permission);
-    return { message: `Permission ${permission.code} deleted` };
+    try {
+      const permission = await this.findOne(id);
+      await this.permissionRepository.remove(permission);
+      return { message: `Permission ${permission.code} deleted` };
+    } catch (error) {
+      throw new PermissionsException(
+        'Permission not deleted',
+        PermissionsErrorCodes.DELETE_PERMISSION_FAILED,
+        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   async findOne(id: string): Promise<PermissionEntity> {
     const permission = await this.permissionRepository.findOneById(id);
-    if (!permission) throw new NotFoundException('Permission not found');
+    if (!permission) throw new PermissionsException('Permission not found', PermissionsErrorCodes.PERMISSION_NOT_FOUND, HttpStatus.NOT_FOUND);
     return permission;
   }
 
   async findAll(): Promise<PermissionEntity[]> {
-    return await this.permissionRepository.findAll();
+    try {
+      return await this.permissionRepository.findAll();
+    } catch (error) {
+      throw new PermissionsException(
+        'Permissions not found',
+        PermissionsErrorCodes.PERMISSION_NOT_FOUND,
+        error.status ?? HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
