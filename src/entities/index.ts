99a3bf4c4cc56c193a1db858277ee5a404de2496
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-16 19:43:07 -0300

    Chore: fix formatting in entities array definition

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -5,4 +5,11 @@ import { ApiKeyEntity } from './api-key.entity';
 import { OutboxEntity } from 'src/entities/outbox.entity';
 import { EndpointPermissionRulesEntity } from 'src/entities/endpoint-permission-rules.entity';
 
-export const entities = [UserEntity, RoleEntity, PermissionEntity, ApiKeyEntity, OutboxEntity, EndpointPermissionRulesEntity];
+export const entities = [
+  UserEntity,
+  RoleEntity,
+  PermissionEntity,
+  ApiKeyEntity,
+  OutboxEntity,
+  EndpointPermissionRulesEntity,
+];
