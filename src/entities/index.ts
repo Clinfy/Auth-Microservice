32e0037068ed46a6f487f2206c4504597a9e2ec2
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-16 15:55:01 -0300

    Chore: include EndpointPermissionRulesEntity in entities array

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -3,5 +3,6 @@ import { RoleEntity } from './role.entity';
 import { PermissionEntity } from './permission.entity';
 import { ApiKeyEntity } from './api-key.entity';
 import { OutboxEntity } from 'src/entities/outbox.entity';
+import { EndpointPermissionRulesEntity } from 'src/entities/endpoint-permission-rules.entity';
 
-export const entities = [UserEntity, RoleEntity, PermissionEntity, ApiKeyEntity, OutboxEntity];
+export const entities = [UserEntity, RoleEntity, PermissionEntity, ApiKeyEntity, OutboxEntity, EndpointPermissionRulesEntity];
