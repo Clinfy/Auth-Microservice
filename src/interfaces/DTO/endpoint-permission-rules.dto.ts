c7454ddd1b4df19794445ff1bc1056b78afd2aa0
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-16 20:04:31 -0300

    Chore: fix formatting in `CreateEndpointPermissionRulesDTO` decorators

--- src/interfaces/DTO/endpoint-permission-rules.dto.ts
+++ src/interfaces/DTO/endpoint-permission-rules.dto.ts
@@ -1,8 +1,8 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateEndpointPermissionRulesDTO {
-  @IsNotEmpty({ message: 'This field is required'})
-  @IsString({message: 'This field must be a string'})
+  @IsNotEmpty({ message: 'This field is required' })
+  @IsString({ message: 'This field must be a string' })
   endpoint_key_name: string;
 }
 
